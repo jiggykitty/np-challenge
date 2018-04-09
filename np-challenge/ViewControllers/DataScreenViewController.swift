9324cab97220d1a064f416e57a0029efed7e2c6b
jiggybear <cagri.sahan@gmail.com>
2018-04-09 13:33:25 -0500

    outlets

---
+++ np-challenge/ViewControllers/DataScreenViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DataScreenViewController.swift
+//  np-challenge
+//
+//  Created by Cagri Sahan on 4/9/18.
+//  Copyright © 2018 Cagri Sahan. All rights reserved.
+//
+
+import UIKit
+
+class DataScreenViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dataTable: UITableView!
+    @IBOutlet weak var drugTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
